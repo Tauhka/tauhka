efa91c008ad1cee30e8bd21549c26088c7dc34de
Juhapekka Piiroinen <juhapekka.piiroinen@criticalforce.fi>
2020-10-27 18:12:46 +0200

    Disable cert related things for UI automation.

--- tauhka/__init__.py
+++ tauhka/__init__.py
@@ -1,6 +1,6 @@
 __name__ = "tauhka"
 __all__ = ["testcase"]
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 __author__ = "Juhapekka Piiroinen"
 __copyright__ = "Copyright (C) 2020 Juhapekka Piiroinen"
 __license__ = "MIT"
